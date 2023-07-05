6eeeddd9b5b9e5fe93171718647cad1a01492639
Dhanush Adithya <k.b.dhanush02@gmail.com>
2023-07-05 13:32:11 +0530

    Updated Front-End

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SearchPage from "./pages/SearchPage/SearchPage";
@@ -18,3 +19,25 @@ function App() {
 }
 
 export default App;
+=======
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import MovieInfo from "./pages/MovieInfo/MovieInfo";
+import Landing from "./pages/Landing/Landing";
+import "../src/css/reset.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Landing} />
+        <Route path="/search" Component={SearchPage} />
+        <Route path="/movie/:id" Component={MovieInfo} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+>>>>>>> origin/main
--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import "../Landing/landing.css";
 
@@ -19,3 +20,26 @@ const Landing = () => {
 };
 
 export default Landing;
+=======
+import React from "react";
+import "../Landing/landing.css";
+
+console.log(process.env.REACT_APP_API_KEY);
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <div className="landing-container">
+        <h1 className="landing-main">
+          DISCOVER YOUR NEW FAVORAITE <br /> FILM TODAY
+        </h1>
+        <a href="/search" className="landing-secondary">
+          TRY IT NOW
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
+>>>>>>> origin/main
